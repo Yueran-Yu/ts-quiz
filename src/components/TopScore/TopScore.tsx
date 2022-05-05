b46432c27fcd60bee0af14ecef959dbc75fa1fe8
GraceYu <librayuyueran@gmail.com>
2022-05-04 22:12:10 -0500

    remove text from topScore

--- src/components/TopScore/TopScore.tsx
+++ src/components/TopScore/TopScore.tsx
@@ -7,7 +7,7 @@ export const TopScore = () => {
 	const max = parentRecords.reduce((prev: RecordsProps, curr) => (parseFloat(prev.highest) >= parseFloat(curr.highest) ? prev : curr), {} as RecordsProps)
 	return (
 		<TopScoreWrapper>
-			<h4><span>Highest</span> Score within Score Within 10 days</h4>
+			<h4><span>Highest</span> Score Within 10 days</h4>
 			<div>Date: <span>{max.date}</span> &nbsp;&nbsp; Score: <span>{max.highest} %</span></div>
 		</TopScoreWrapper>
 	)
